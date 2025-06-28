f26455631bedc105647addf47fc9078550c154d3
todomatu <macbook.nk96@gmail.com>
2025-06-29 02:51:44 +0900

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,11 @@ class Message < ApplicationRecord
   has_one_attached :image
 
   validates :content,presence: true,unless: :was_attached?
+  # validates :image,presence: true,unless: :content_present?
   def was_attached?
     self.image.attached?
   end
+  # def content_present?
+  #   self.content.present?
+  # end
 end
